432654799e6d7780d202922aa7caaa0dc45820d0
jchillah <jchillah@gmail.com>
2024-06-24 13:01:43 +0200

    done

--- lib/src/features/gold/presentation/gold_screen.dart
+++ lib/src/features/gold/presentation/gold_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:stream_gold_rate/src/features/gold/data/fake_gold_api.dart';
 
 class GoldScreen extends StatelessWidget {
   const GoldScreen({super.key});
@@ -7,7 +8,6 @@ class GoldScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     /// Platzhalter für den Goldpreis
     /// soll durch den Stream `getGoldPriceStream()` ersetzt werden
-    const double goldPrice = 69.22;
 
     return SafeArea(
       child: Scaffold(
@@ -23,12 +23,32 @@ class GoldScreen extends StatelessWidget {
               const SizedBox(height: 20),
               // TODO: Verwende einen StreamBuilder, um den Goldpreis live anzuzeigen
               // statt des konstanten Platzhalters
-              Text(
-                NumberFormat.simpleCurrency(locale: 'de_DE').format(goldPrice),
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineLarge!
-                    .copyWith(color: Theme.of(context).colorScheme.primary),
+              StreamBuilder<double>(
+                stream: getGoldPriceStream(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData &&
+                          snapshot.connectionState == ConnectionState.done ||
+                      snapshot.connectionState == ConnectionState.active) {
+                    // FALL 1: Future ist komplett und hat Daten!
+                    final goldPrice = snapshot.data;
+
+                    return Text(
+                      NumberFormat.simpleCurrency(locale: 'de_DE')
+                          .format(goldPrice),
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineLarge!
+                          .copyWith(
+                              color: Theme.of(context).colorScheme.primary),
+                    );
+                  } else if (snapshot.connectionState != ConnectionState.done) {
+                    // FALL 2: Sind noch im Ladezustand
+                    return const CircularProgressIndicator();
+                  } else {
+                    // FALL 3: Es gab nen Fehler
+                    return const Icon(Icons.error);
+                  }
+                },
               ),
             ],
           ),
